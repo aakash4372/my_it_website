c368b2723dcf779e91307e10261522813aa627bb
Aakash <aakashaakash0013@gmail.com>
2024-08-29 14:08:01 +0530

    project

--- src/Bookform/book.js
+++ src/Bookform/book.js
@@ -44,26 +44,27 @@ const Bookpopup = ({ show, handleClose, title }) => {
       <Modal.Body>
         <form className="form" onSubmit={handleSubmit}>
           <label htmlFor="name" className="label">Name</label>
-          <input type="text" id="name" name="name" required="" className="input"/>
+          <input type="text" id="name" name="name" required="" className="input" placeholder='Enter Your Name'/>
 
           <label htmlFor="email" className="label">Email</label>
-          <input type="email" id="email" name="email" required="" className="input"/>
+          <input type="email" id="email" name="email" required="" className="input" placeholder='Enter Your Email'/>
 
           <label htmlFor="phone" className="label">Phone</label>
-          <input type="tel" id="phone" name="phone" required="" className="input"/>
+          <input type="tel" id="phone" name="phone" required="" className="input"placeholder='Enter Your Phone'/>
 
           <label htmlFor="city" className="label">City</label>
-          <input type="text" id="city" name="city" required="" className="input"/>
+          <input type="text" id="city" name="city" required="" className="input" placeholder='Enter Your city'/>
 
           <label htmlFor="State" className="label">State</label>
-          <input type="text" id="State" name="State" required="" className="input"/>
+          <input type="text" id="State" name="State" required="" className="input" placeholder='Enter Your state'/>
 
           <label htmlFor="Course" className="label">Course</label>
-          <select name="cars" id="cars" className="input" required>
+          <select name="course" id="course" className="input" required>
             <option value="" selected disabled>Select Course</option>
             <option value="Full Stack">Full Stack Development</option>
-            <option value="Back-end">Back-End Development</option>
-            <option value="Front-end">Front-End Development</option>
+            <option value="Back-end">Python</option>
+            <option value="Front-end">Java</option>
+            <option value="Front-end">Software Testing</option>
           </select>
 
           <button type="submit" className="submit">Submit</button>
--- src/footer/footer.js
+++ src/footer/footer.js
@@ -30,49 +30,73 @@ const Footer = () => {
           <div className="row">
             <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
               <h5 className="text-uppercase">General Enquires</h5>
-              <form>
+              <form className="mt-4">
                 <div className="row">
                   <div className="col-md-6">
                     <input
                       type="text"
-                      className="form-control mb-2"
+                      id="name"
+                      name="name"
+                      required=""
+                      className="input"
                       placeholder="Name"
                     />
                   </div>
+
                   <div className="col-md-6">
                     <input
                       type="email"
-                      className="form-control mb-2"
+                      id="email"
+                      name="email"
+                      required=""
+                      className="input"
                       placeholder="Email"
                     />
                   </div>
+
                   <div className="col-md-6">
                     <input
-                      type="text"
-                      className="form-control mb-2"
+                      type="tel"
+                      id="phone"
+                      name="phone"
+                      required=""
+                      className="input"
                       placeholder="Phone"
                     />
                   </div>
+
                   <div className="col-md-6">
                     <input
                       type="text"
-                      className="form-control mb-2"
-                      placeholder="City"
+                      id="city"
+                      name="city"
+                      required=""
+                      className="input"
+                      placeholder="city"
                     />
                   </div>
+
                   <div className="col-md-6">
                     <input
                       type="text"
-                      className="form-control mb-2"
-                      placeholder="State"
+                      id="State"
+                      name="State"
+                      required=""
+                      className="input"
+                      placeholder="state"
                     />
                   </div>
+
                   <div className="col-md-6">
-                    <input
-                      type="text"
-                      className="form-control mb-2"
-                      placeholder="Course"
-                    />
+                    <select name="course" id="course" className="input" required>
+                      <option value="" selected disabled>
+                        Select Course
+                      </option>
+                      <option value="Full Stack">Full Stack Development</option>
+                      <option value="Back-end">Python</option>
+                      <option value="Front-end">Java</option>
+                      <option value="Front-end">Software Testing</option>
+                    </select>
                   </div>
                 </div>
                 <button type="submit" className="btn btn-primary mt-2">
@@ -81,22 +105,23 @@ const Footer = () => {
               </form>
             </div>
             <div className="col-lg-4 col-md-6 mb-4 mb-md-0 text-start text-lg-start">
-              <h5 className="text-uppercase">Location</h5>
-              <ul className="list-unstyled">
-                <li>
-                  <FontAwesomeIcon icon={faLocationDot} /> No 36, Natesan Nagar,
-                  Pondicherry, 605005
+              <h5 className="text-uppercase ps-4">Location</h5>
+              <ul className="list-unstyled  ps-4 mt-4">
+                <li className="my-2">
+                  <a href="https://maps.app.goo.gl/J3iWiB2Vo5Fwfb5m6" target="_blank" rel="noreferrer" className="location-section "><FontAwesomeIcon icon={faLocationDot} /> No 36, Natesan Nagar,
+                  Pondicherry, 605005</a>
                 </li>
-                <li>
-                  <FontAwesomeIcon icon={faPhone} /> +91 94423 69773
+                <li className="my-2">
+                <a href="tel:81898 91619" target="_blank" rel="noreferrer" className="location-section"><FontAwesomeIcon icon={faPhone} /> +91 81898 91619</a>
                 </li>
-                <li>
-                  <FontAwesomeIcon icon={faEnvelope} /> myitcareer2024@gmail.com
+                <li className="my-2">
+                <a href="mailto: myitcareer2024@gmail.com" rel="noreferrer" target="_blank" className="location-section"><FontAwesomeIcon icon={faEnvelope} /> myitcareer2024@gmail.com</a>
                 </li>
               </ul>
               <div>
                 <ul class="icon_wrapper">
-                  <li class="icon facebook">
+                 <a href="https://www.facebook.com/myitcareerin" target="_blank"  rel="noreferrer" className="location-section">
+                 <li class="icon facebook">
                     <span class="tooltip">Facebook</span>
                     <svg
                       viewBox="0 0 320 512"
@@ -107,6 +132,8 @@ const Footer = () => {
                       <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
                     </svg>
                   </li>
+                 </a>
+                 <a href="https://x.com/myitcareer9089" target="_blank" rel="noreferrer" className="location-section">
                   <li class="icon twitter">
                     <span class="tooltip">Twitter</span>
                     <svg
@@ -121,6 +148,8 @@ const Footer = () => {
                       />
                     </svg>
                   </li>
+                  </a>
+                  <a href="https://www.instagram.com/myitcareerin?igsh=MjgwZ2lkMmJ6Mml3" target="_blank"  rel="noreferrer" className="location-section">
                   <li class="icon instagram">
                     <span class="tooltip">Instagram</span>
                     <svg
@@ -133,6 +162,8 @@ const Footer = () => {
                       <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
                     </svg>
                   </li>
+                  </a>
+                  <a href="https://www.youtube.com/@MyitCarrer" target="_blank"  rel="noreferrer" className="location-section">
                   <li class="icon youtube">
                     <span class="tooltip">Youtube</span>
                     <svg
@@ -146,7 +177,8 @@ const Footer = () => {
                         d="M23 9.71a8.5 8.5 0 0 0-.91-4.13a2.92 2.92 0 0 0-1.72-1A78 78 0 0 0 12 4.27a79 79 0 0 0-8.34.3a2.87 2.87 0 0 0-1.46.74c-.9.83-1 2.25-1.1 3.45a48 48 0 0 0 0 6.48a9.6 9.6 0 0 0 .3 2a3.14 3.14 0 0 0 .71 1.36a2.86 2.86 0 0 0 1.49.78a45 45 0 0 0 6.5.33c3.5.05 6.57 0 10.2-.28a2.9 2.9 0 0 0 1.53-.78a2.5 2.5 0 0 0 .61-1a10.6 10.6 0 0 0 .52-3.4c.04-.56.04-3.94.04-4.54M9.74 14.85V8.66l5.92 3.11c-1.66.92-3.85 1.96-5.92 3.08"
                       />
                     </svg>
-                  </li>
+                  </li></a>
+                  <a href="https://www.linkedin.com/in/myit-career/" target="_blank"  rel="noreferrer"  className="location-section">
                   <li class="icon linkedin">
                     <span class="tooltip">LinkedIn</span>
                     <svg
@@ -160,7 +192,7 @@ const Footer = () => {
                         d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
                       />
                     </svg>{" "}
-                  </li>
+                  </li></a>
                 </ul>
               </div>
             </div>
